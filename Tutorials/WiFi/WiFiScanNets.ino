01873ff448861ea37a48fabb4033919065028164
electrorex <151402332+electrorex@users.noreply.github.com>
2023-11-25 10:38:54 -0600

    Update scna nets file

--- Tutorials/WiFi/WiFiScanNets.ino
+++ Tutorials/WiFi/WiFiScanNets.ino
@@ -22,24 +22,6 @@ void printWiFiMac() {
   printMacAddress(mac);
 }
 
-float mvolts; //millivolts read from any analog pin
-float Batv;
-
-//Function to read voltage on some analog pin and output in millivolts
-float readVolts(int pin) {
-  analogReadResolution(12);
-  mvolts = analogRead(pin);
-  mvolts *= 2;
-  mvolts *= (3.3 / 4096);
-  mvolts *= 1000;
-  return mvolts;
-}
-
-//Function to read Battery voltage
-void readBat() {
-  Batv = readVolts(A4);
-}
-
 void listNetworks() {
   // scan for nearby networks:
   Serial.println("** Scan Networks **");
