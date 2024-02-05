ef48324ea2510c5dd2ee5de583c2b3ad133c0404
electrorex <151402332+electrorex@users.noreply.github.com>
2024-02-05 09:16:24 -0600

    Update ReadEXO_RS232.ino

--- Tutorials/EXO/ReadEXO_RS232/ReadEXO_RS232.ino
+++ Tutorials/EXO/ReadEXO_RS232/ReadEXO_RS232.ino
@@ -1,4 +1,4 @@
-/*This sketch was written for the Electrorex Telogger tutorials
+/*This sketch was written for the Electrorex Telogger Tutorial 6
 -Todd R. Miller, February 5, 2024
  
 This sketch will read an EXO2 sonde through the YSI DCP adaptor on the Telelogger's RS232 serial port, TX2/RX2.
